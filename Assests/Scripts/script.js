3ce7b4c3c0df9b4bdb3e70a5c7117e627e218b79
BrandonVA <codebva@gmail.com>
2020-10-04 11:59:36 -0700

    Added a couple function and event to handle adding new citys and getting data from existing cities

--- Assests/Scripts/script.js
+++ Assests/Scripts/script.js
@@ -1,1 +1,49 @@
-console.log('Hello World');
+$(document).ready(function() {
+    console.log('jquery!!!')
+
+
+
+const addActiveCity = (event) => {
+    event.preventDefault();
+    let testThis = event.target.innerText
+    console.log(testThis);
+}
+
+const creatCityItem = (event) => {
+    event.preventDefault();
+    let newCityIemEl = $('<li>');
+    let newCityText = $('#city-input').val();
+
+
+    newCityIemEl.addClass('list-group-item')
+    .text(newCityText);
+    $('.list-group').append(newCityIemEl);
+
+}
+
+
+
+$('li').on('click', addActiveCity)
+$('#searchCity').on('click', creatCityItem)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
